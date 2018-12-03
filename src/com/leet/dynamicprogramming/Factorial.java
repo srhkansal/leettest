dc503ed5fbb0483a74c57b22778a6fa060e1f2a0
srhkansal <srh.kansal@gmail.com>
2018-12-02 19:17:30 -0500

    factorial commit

---
+++ src/com/leet/dynamicprogramming/Factorial.java
@@ -0,0 +1,23 @@
+package com.leet.dynamicprogramming;
+
+/**
+ * Problem statement : calculate the factorial value for a given  umber
+ */
+public class Factorial {
+    public static void main(String []args) {
+
+        int number = 10;
+        int memo[] = new int[number+1];
+        int factorial = Factorial.fact(number, memo);
+        System.out.println("factorial.. "+factorial);
+    }
+
+    public static int fact(int n, int []memo){
+        if(n<=1 )
+            return 1;
+        else if(memo[n]!=0)
+            return memo[n];
+        else
+            return n*fact(n-1, memo);
+    }
+}
